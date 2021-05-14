1a477f0bfe40c8118e2c0c102777865804c20e00
pubudi <pubudiherath@gmail.com>
2021-05-14 19:57:02 +0530

    Add mongo db connection

---
+++ backend/config/db.js
@@ -0,0 +1,30 @@
+require('dotenv').config(); // the place we store mongo uri.
+const mongoose = require('mongoose');
+const connectDB = async () => {
+  try {
+    // add connect methode with the require settings object and mongo atlas uri .
+    // doc : https://mongoosejs.com/docs/connections.html .
+    const MONGO_URI ='mongodb+srv://user-123:user-123@shopping-cart-mern.epqdy.mongodb.net/shop?retryWrites=true&w=majority'
+    await mongoose.connect(process.env.MONGO_URI || MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true, // (node:14872) DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead. **fixed.
+    });
+
+    console.log('MongoDB connection SUCCESS');
+  } catch (error) {
+    console.error('MongoDB connection FAIL');
+    process.exit(1); // existing the server with the exit code 1.
+  }
+};
+
+module.exports = connectDB;
+
+// TO CREATE MONGO ATLAS DB
+// 1 - go to https://www.mongodb.com/cloud/atlas2 and create mongo atlas account.
+// 2 - create a project.
+// 3 - create cluster.
+// 4 - config database access (create user add user existing user).
+// 5 - config network access (allow the access as you wish).
+// 6 - in cluster click connect and select connect your application, select driver with node vertion and get the connection uri (connectonString).
+// 7 - change password to your password and give a name to your document(database). the collection name an be any meaningfull name.
