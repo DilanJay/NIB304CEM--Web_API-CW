7c28d3fc6eeecaeb26b5c42fb17c7b79da8a6fae
pubudi <pubudiherath@gmail.com>
2021-05-14 20:01:48 +0530

    Added Node + Express Server and  GET, PUT Product Functions

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+require('dotenv').config(); // import .env
+const express = require('express'); // import express
+const connectDB = require('./config/db');
+
+connectDB();
+// call the mongodb fucntion exported from the modngo db config file db.js
+
+const app = express();
+
+app.use(express.json()); // this convert request to json
+
+// command to start backe end server (if you are in main folder ) => node backend/server.js
+const PORT = process.env.PORT || 5000; // set the port
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); // log to console.
