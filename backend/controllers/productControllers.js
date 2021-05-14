a4a96a9beb9a00f50375fe9faaac996d5c17b2e4
Heshani chamalka <63486300+Heshani-96@users.noreply.github.com>
2021-05-14 21:17:52 +0530

    add GET product route

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,6 +1,16 @@
 const Product = require('../models/Product');
 ObjectID = require('mongodb').ObjectID;
 
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({}); // get all products
+    res.status(200).json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
+
 const getProductById = async (req, res) => {
     try {
       const product = await Product.findById(req.params.id); // get individual product
@@ -84,6 +94,7 @@ const updateProduct = async (req, res) => {
   };
 
   module.exports = {
+    getProducts,
     getProductById,
     updateProduct,
     addProduct,
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const {
+   getProducts,
     getProductById,
     addProduct,
     updateProduct,
    
   } = require('../controllers/productControllers'); // named impots
 
+  router.get('/', getProducts);
 
   router.get('/:id', getProductById);
   router.post('/', addProduct);
