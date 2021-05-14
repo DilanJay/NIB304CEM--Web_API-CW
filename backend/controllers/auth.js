d46dcffc4361257a109b6d330ca5018fb7093a57
HeshanHH <heshanwijerathna01@gmail.com>
2021-05-14 22:51:55 +0530

    Add DELETE Product Route And API Documentation for apis

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -138,40 +138,6 @@ exports.forgotPassword = async (req, res, next) => {
   }
 };
 
-// @desc    Reset User Password
-exports.resetPassword = async (req, res, next) => {
-  // Compare token in URL params to hashed token
-  const resetPasswordToken = crypto
-    .createHash('sha256')
-    .update(req.params.resetToken)
-    .digest('hex');
-
-  try {
-    const user = await User.findOne({
-      resetPasswordToken,
-      resetPasswordExpire: { $gt: Date.now() }, // theck token isvalide?
-    });
-
-    if (!user) {
-      return next(new ErrorResponse('Invalid Token', 400));
-    }
-
-    user.password = req.body.password;
-    user.resetPasswordToken = undefined;
-    user.resetPasswordExpire = undefined;
-
-    await user.save();
-
-    res.status(201).json({
-      success: true,
-      data: 'Password Updated Success',
-      token: user.getSignedJwtToken(),
-    });
-  } catch (err) {
-    next(err);
-  }
-};
-
 // create a separeate function to sendToken with response and status code.
 const sendToken = (user, statusCode, res) => {
   const token = user.getSignedJwtToken();
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,16 +1,253 @@
 const express = require('express');
 const router = express.Router();
 const {
-   getProducts,
-    getProductById,
-    addProduct,
-    updateProduct,
-   
-  } = require('../controllers/productControllers'); // named impots
+  getProducts,
+  getProductById,
+  deleteProduct,
+  addProduct,
+  updateProduct,
+} = require('../controllers/productControllers'); // named impots
 
-  router.get('/', getProducts);
+//https://github.com/Surnet/swagger-jsdoc/blob/v6/examples/app/routes.js
 
-  router.get('/:id', getProductById);
-  router.post('/', addProduct);
-  router.put('/:id', updateProduct);
-  module.exports = router;
+/**
+ * @swagger
+ * /api/products:
+ *  get:
+ *    description: Use to GET all products
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ *      '500':
+ *        description: Server Error
+ */
+
+router.get('/', getProducts);
+
+// http//:loacalhost:5000/api/products/60918201eb80df4090379a80
+
+/**
+ * @swagger
+ * /api/products/{id}:
+ *   parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *        type: integer
+ *      required: true
+ *      description: Numeric ID of the user to GET
+ *   get:
+ *     summary: Retrieve a single JSONPlaceholder product.
+ *     description: Retrieve a single JSONPlaceholder product. Can be used to populate a product profile when prototyping or testing an API.
+ *     responses:
+ *       200:
+ *         description: A single product.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       description: The product ID.
+ *                       example: 0
+ *                     name:
+ *                       type: string
+ *                       description: The product's name.
+ *                       example: Iphone 12
+ *                     imageUrl:
+ *                       type: string
+ *                       description: The product's Image Url.
+ *                       example: http://example.com/123
+ *                     description:
+ *                       type: string
+ *                       description: The product's description.
+ *                       example: abcd
+ *                     price:
+ *                       type: integer
+ *                       description: The product's price.
+ *                       example: $100.10
+ *                     countInStock:
+ *                       type: integer
+ *                       description: The product's Count in stock.
+ *                       example: 10
+ */
+
+router.get('/:id', getProductById);
+
+/**
+ * @swagger
+ * /api/products:
+ *   post:
+ *     summary: Retrieve a single JSONPlaceholder product.
+ *     description: Retrieve a single JSONPlaceholder product. Can be used to populate a product profile when prototyping or testing an API.
+ *     requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *               type: object
+ *               properties:
+ *                  data:
+ *                    type: object
+ *                    properties:
+ *                      name:
+ *                        type: string
+ *                        description: The product's name.
+ *                        example: Iphone 12
+ *                      imageUrl:
+ *                        type: string
+ *                        description: The product's Image Url.
+ *                        example: http://example.com/123
+ *                      description:
+ *                        type: string
+ *                        description: The product's description.
+ *                        example: abcd
+ *                      price:
+ *                        type: integer
+ *                        description: The product's price.
+ *                        example: $100.10
+ *                      countInStock:
+ *                        type: integer
+ *                        description: The product's Count in stock.
+ *                        example: 10
+ *   responses:
+ *     '200':
+ *        description: A single product.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                  data:
+ *                    type: object
+ *                    properties:
+ *                      name:
+ *                        type: string
+ *                        description: The product's name.
+ *                        example: Iphone 12
+ *                      imageUrl:
+ *                        type: string
+ *                        description: The product's Image Url.
+ *                        example: http://example.com/123
+ *                      description:
+ *                        type: string
+ *                        description: The product's description.
+ *                        example: abcd
+ *                      price:
+ *                        type: integer
+ *                        description: The product's price.
+ *                        example: $100.10
+ *                      countInStock:
+ *                        type: integer
+ *                        description: The product's Count in stock.
+ *                        example: 10
+ *     '500':
+ *        description: Server Error
+ */
+
+router.post('/', addProduct);
+
+/**
+ * @swagger
+ * /api/products/{id}:
+ *   parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *        type: integer
+ *      required: true
+ *      description: Numeric ID of the user to GET
+ *   put:
+ *     summary: Retrieve a single JSONPlaceholder product.
+ *     description: Retrieve a single JSONPlaceholder product. Can be used to populate a product profile when prototyping or testing an API.
+ *     requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *               type: object
+ *               properties:
+ *                  data:
+ *                    type: object
+ *                    properties:
+ *                      name:
+ *                        type: string
+ *                        description: The product's name.
+ *                        example: Iphone 12
+ *                      imageUrl:
+ *                        type: string
+ *                        description: The product's Image Url.
+ *                        example: http://example.com/123
+ *                      description:
+ *                        type: string
+ *                        description: The product's description.
+ *                        example: abcd
+ *                      price:
+ *                        type: integer
+ *                        description: The product's price.
+ *                        example: $100.10
+ *                      countInStock:
+ *                        type: integer
+ *                        description: The product's Count in stock.
+ *                        example: 10
+ *   responses:
+ *     '200':
+ *        description: A single product.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                  data:
+ *                    type: object
+ *                    properties:
+ *                      name:
+ *                        type: string
+ *                        description: The product's name.
+ *                        example: Iphone 12
+ *                      imageUrl:
+ *                        type: string
+ *                        description: The product's Image Url.
+ *                        example: http://example.com/123
+ *                      description:
+ *                        type: string
+ *                        description: The product's description.
+ *                        example: abcd
+ *                      price:
+ *                        type: integer
+ *                        description: The product's price.
+ *                        example: $100.10
+ *                      countInStock:
+ *                        type: integer
+ *                        description: The product's Count in stock.
+ *                        example: 10
+ *     '500':
+ *        description: Server Error
+ */
+router.put('/:id', updateProduct);
+
+/**
+ * @swagger
+ * /api/products/{id}:
+ *   parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *        type: integer
+ *      required: true
+ *      description: Numeric ID of the user to DELETE
+ *   delete:
+ *     summary: Retrieve a single JSONPlaceholder product.
+ *     description: Retrieve a single JSONPlaceholder product. Can be used to populate a product profile when prototyping or testing an API.
+ *   responses:
+ *     200:
+ *       description: A single product.
+ *     '500':
+ *       description: Server Error
+ */
+router.delete('/:id', deleteProduct);
+module.exports = router;
