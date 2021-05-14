e8fce242a1546f994496c6205acc1ceacdfd919f
HeshanHH <heshanwijerathna01@gmail.com>
2021-05-14 22:34:55 +0530

    Import Redux store and Providers

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// wrapping the whole app using privider we can access the store in anywhre of the application.
+import { Provider } from 'react-redux';
+import store from './redux/store';
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    ,
+  </Provider>,
   document.getElementById('root')
 );
 reportWebVitals();
