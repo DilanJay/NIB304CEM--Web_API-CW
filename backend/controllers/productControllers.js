f1120be3495bf7bbb19eb55e90810c8839343345
DilanJay <dilan.pramodya97@gmail.com>
2021-05-14 20:38:13 +0530

    Add POST request to Product

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -21,6 +21,35 @@ const getProductById = async (req, res) => {
 //   "countInStock": 5
 // }
 
+// @Body
+// {
+//     "name": "PlayStation 6",
+//     "imageUrl":"https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80",
+//     "description":"PlayStation 6 (PS6) is a home video game console developed by Sony Interactive Entertainment. Announced in 2019 as the successor to the PlayStation 4, the PS5 was released on November 12, 2020 in Australia, Japan, New Zealand, North America, Singapore, and South Korea, and November 19, 2020 onwards in other major markets except China and India.",
+//     "price": 499,
+//     "countInStock": 15
+// }
+
+const addProduct = async (req, res) => {
+  try {
+    let requestBody = req.body;
+
+    if (requestBody != null) {
+      const product = await Product.create({ ...requestBody });
+      console.log(req);
+      res.status(200).json(requestBody);
+    } else {
+      res.status(406).json({
+        data: requestBody,
+        message: 'please fill all requires fields',
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error', err: error });
+  }
+};
+
 const updateProduct = async (req, res) => {
     try {
       // https://docs.mongodb.com/manual/reference/method/db.collection.updateOne/#mongodb-method-db.collection.updateOne
@@ -57,4 +86,5 @@ const updateProduct = async (req, res) => {
   module.exports = {
     getProductById,
     updateProduct,
+    addProduct,
   }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const {
     getProductById,
+    addProduct,
     updateProduct,
+   
   } = require('../controllers/productControllers'); // named impots
 
 
   router.get('/:id', getProductById);
+  router.post('/', addProduct);
   router.put('/:id', updateProduct);
-
   module.exports = router;
